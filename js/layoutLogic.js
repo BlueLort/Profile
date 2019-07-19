c8dd52ff2177d1b4594bf2c3687b7223bb726e4d
BlueLort <emaranasr@gmail.com>
2019-07-20 00:05:22 +0200

    fixed a bug in switching html

--- js/layoutLogic.js
+++ js/layoutLogic.js
@@ -4,12 +4,17 @@ const PROJECTS='content/projects.html';
 const LIVE_PROJECTS='content/liveProjects.html';
 const RESUME='content/resume.html';
 //============================================
-$( "#content" ).load(HOME);
+switch(document.location.hash){
+    case "#home":$( "#content" ).load(HOME); break;
+    case "#projects":$( "#content" ).load(PROJECTS); break;
+    case "#live_projects":$( "#content" ).load(LIVE_PROJECTS); break;
+    case "#resume":$( "#content" ).load(RESUME); break;
+}
+
 
 function fixActice(object,newContentURL,newTitle){
     $(".active").removeAttr("class");
     $(object).addClass("active");
-    console.log(newContentURL);
     document.title = "Omar Nasr - "+newTitle;
     $('#content').animate({ opacity: '0.0'}, { duration: 300, queue: false, complete: function() {
         $("#content").load(newContentURL);
