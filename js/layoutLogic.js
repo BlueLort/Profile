50b41273f0764fe09b71fe9949e2fd3996f7159a
BlueLort <emaranasr@gmail.com>
2019-07-19 21:58:59 +0200

    updating the home page

--- js/layoutLogic.js
+++ js/layoutLogic.js
@@ -1,8 +1,34 @@
-const Http = new XMLHttpRequest();
-const url='../content/home.html';
-Http.open("GET", url);
-Http.send();
+//============URLS===========================
+const HOME='content/home.html';
+const PROJECTS='content/projects.html';
+const LIVE_PROJECTS='content/liveProjects.html';
+const RESUME='content/resume.html';
+//============================================
+$( "#content" ).load(HOME);
 
-Http.onreadystatechange = (e) => {
-	$( "#content" ).html( Http.responseText );
+function fixActice(object,newContentURL,newTitle){
+    $(".active").removeAttr("class");
+    $(object).addClass("active");
+    document.title = "Omar Nasr - "+newTitle;
+    $('#content').animate({ opacity: '0.0'}, { duration: 300, queue: false, complete: function() {
+        $("#content").load(newContentURL);
+        $('#content').animate({ opacity: '1.0'}, { duration: 300, queue: false});
+    }}
+    );
 }
+$("#menu #HOME").click(function(event){
+    if($(".active").attr("id")=="HOME")return;
+    fixActice(this,HOME,"Home");
+})
+$("#menu #PROJECTS").click(function(event){
+    if($(".active").attr("id")=="PROJECTS")return;
+    fixActice(this,PROJECTS,"Projects");
+})
+$("#menu #LIVE_PROJECTS").click(function(event){
+    if($(".active").attr("id")=="LIVE_PROJECTS")return;
+    fixActice(this,LIVE_PROJECTS,"Live Projects");
+})
+$("#menu #RESUME").click(function(event){
+    if($(".active").attr("id")=="RESUME")return;
+    fixActice(this,RESUME,"Resume");
+})
